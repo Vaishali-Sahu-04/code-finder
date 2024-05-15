2c6923c13fff7c6ad323e7b930d98f3d94f80da8
Vaishali-Sahu-04 <vaishusahuone@gmail.com>
2024-05-15 22:57:43 +0530

    github api integrated

--- frontend/src/components/ProfileInfo.jsx
+++ frontend/src/components/ProfileInfo.jsx
@@ -3,23 +3,24 @@ import { RiGitRepositoryFill, RiUserFollowFill, RiUserFollowLine } from "react-i
 import { FaXTwitter } from "react-icons/fa6";
 import { TfiThought } from "react-icons/tfi";
 import { FaEye } from "react-icons/fa";
+import { formatMemberSince } from "../utils/function";
 
-const ProfileInfo = () => {
-	const userProfile = {
-		avatar_url: "./avatar.png",
-		bio: "👨🏻‍💻👨🏻‍💻👨🏻‍💻",
-		email: "vaishusahuone@gmail.com",
-		followers: 0,
-		following: 0,
-		html_url: "https://github.com/Vaishali-Sahu-04",
-		location: "Somewhere, Earth",
-		name: "Vaishali Sahu",
-		public_gists: 100,
-		public_repos: 100,
-		twitter_username: "vaishalisahu",
-		login: "vaishalisahu",
-	};
-
+const ProfileInfo = ({userProfile}) => {
+	// const userProfile = {
+	// 	avatar_url: "./avatar.png",
+	// 	bio: "👨🏻‍💻👨🏻‍💻👨🏻‍💻",
+	// 	email: "vaishusahuone@gmail.com",
+	// 	followers: 0,
+	// 	following: 0,
+	// 	html_url: "https://github.com/Vaishali-Sahu-04",
+	// 	location: "Somewhere, Earth",
+	// 	name: "Vaishali Sahu",
+	// 	public_gists: 100,
+	// 	public_repos: 100,
+	// 	twitter_username: "vaishalisahu",
+	// 	login: "vaishalisahu",
+	// };
+	const memberSince = formatMemberSince(userProfile?.created_at);
 	return (
 		<div className='lg:w-1/3 w-full flex flex-col gap-2 md:sticky md:top-10'>
 			<div className='bg-glass rounded-lg p-4'>
@@ -31,7 +32,7 @@ const ProfileInfo = () => {
 					{/* View on Github */}
 					<div className='flex gap-2 items-center flex-col'>
 						<a
-							href={userProfile.html_url}
+							href={userProfile?.html_url}
 							target='_blank'
 							rel='noreferrer'
 							className='bg-glass font-medium w-full text-xs p-2 rounded-md cursor-pointer border border-blue-400 flex items-center gap-2'
@@ -74,7 +75,7 @@ const ProfileInfo = () => {
 				{/* Member Since Date */}
 				<div className='my-2'>
 					<p className='text-gray-600 font-bold text-sm'>Member since</p>
-					<p className=''>21 Sep, 2023</p>
+					<p className=''>{memberSince}</p>
 				</div>
 
 				{/* Email Address */}
